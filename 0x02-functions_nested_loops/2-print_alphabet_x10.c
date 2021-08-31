b2f5168c487740c64dde831f8d83bbe6326741a0
Robi000 <88291984+Robi000@users.noreply.github.com>
2021-08-31 16:37:00 +0300

    Create 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+
+#include "main.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void);
+{
+	char c;
+for (int i=0 ; i<11 ; i++){
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');  
+}
+  _putchar('\n');
+}

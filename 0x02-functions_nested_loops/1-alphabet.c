34fb628a273241a00c983872d4115b62bb1a44d4
Robi000 <88291984+Robi000@users.noreply.github.com>
2021-08-31 16:28:21 +0300

    Create 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include <main.h>
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void) {
+int c[] = "abcdefghijklmnopqrstuvwxyz";
+  for (int i=0 ; i <=25 ; i++)
+    _putchar(c[i]);
+  _putchar('\n');
+}

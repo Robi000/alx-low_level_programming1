2aff86c378cd54861db90fdde4e82a0ba6eb5ede
Robi000 <88291984+Robi000@users.noreply.github.com>
2021-11-01 01:21:12 +0300

    little correction on the length part

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -7,14 +7,42 @@
  * @n: bytes to be copied
  * Return: copied string
  */
-char *_strncpy(char *dest, char *src, int n)
+int _strlen(char *str)
 {
-	char *ptr = dest;
+	int length = 0;
+
+	while (*str)
+	{
+		str++;
+		length++;
+	}
+
+	return (length);
+
+}
+
+
+/**
+ *_strcat - concatinates two strings
+ *@dest:destination pointer
+ *@src:pointer to a string
+ *Return: concatinated string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	char *cat = dest + _strlen(dest);
+	int length =  _strlen(dest);
 
 	while (*src && n--)
-		*dest++ = *src++;
-	if (!*src)
-		while (*dest && n--)
-			*dest++ = 0;
-	return (ptr);
+	{
+		*cat = *src;
+		src++;
+		cat++;
+        	length++;
+	}
+	*cat = '\0';
+	cat -= (length);
+	//*dest = *cat;
+
+	return (cat);
 }

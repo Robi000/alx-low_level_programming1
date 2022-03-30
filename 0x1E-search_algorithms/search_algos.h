69929230eb683b7528314694ea1b81c4027c5a4d
Robi000 <88291984+Robi000@users.noreply.github.com>
2022-03-30 22:44:38 +0300

    Update search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,9 +2,10 @@
 #define SEARCH_ALGOS
 
 #include "stdio.h"
+#include "math.h"
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-void print_array(int *array, int lower_bound, int upper_bound)
+void print_array(int *array, int lower_bound, int upper_bound);
 
 #endif

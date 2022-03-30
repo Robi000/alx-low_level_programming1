e88826f3f12932ba97aed56de5a30013deb3cbaa
Robi000 <88291984+Robi000@users.noreply.github.com>
2022-03-30 22:34:51 +0300

    Update 0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,21 +1,25 @@
 #include "search_algos.h"
 
+/**
+ * linear_search - linear search implementation
+ * @array: string
+ * @size: size_t
+ * @value: value
+ * Return: int, index of the value
+ */
+
 int linear_search(int *array, size_t size, int value)
 {
-    int i = 0;
-    while (size > 0)
-    {
-        if (array[i] != value)
-        {
-            printf("Value checked array[%d] = [%d]\n", i, array[i]);
-        }
-        else
-        {
-            printf("Value checked array[%d] = [%d]\n", i, value);
-            return i;
-        }
-        i++;
-        size--;
-    }
-    return -1;
+size_t i;
+
+if (array == NULL)
+	return (-1);
+
+for (i = 0; i < size; i++)
+{
+	printf("Value checked array[%li] = [%i]\n", i, array[i]);
+	if (array[i] == value)
+		return (i);
+}
+return (-1);
 }
